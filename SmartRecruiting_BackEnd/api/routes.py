47f6f938e572806e143e8b8919662b4b52315783
ant38 <antoine.gambro@outlook.com>
2018-02-08 10:57:21 +0100

    routes pour getAll

--- SmartRecruiting_BackEnd/api/routes.py
+++ SmartRecruiting_BackEnd/api/routes.py
@@ -8,10 +8,30 @@ from flask.json import jsonify
 from flask import render_template
 from SmartRecruiting_BackEnd import app
 
-from SmartRecruiting_BackEnd.data import Program, DatabaseManager
+from SmartRecruiting_BackEnd.data import DatabaseManager
 
 dbManager = DatabaseManager()
+
+@app.route('/users')
+def getUsers():
+    return jsonify(dbManager.getAllUsers()), 200
+
+@app.route('/offers')
+def getOffers():
+    return jsonify(dbManager.getAllOffers()), 200
+
+@app.route('/predictions')
+def getPredictions():
+    return jsonify(dbManager.getAllPredictions()), 200
+
+@app.route('/teams')
+def getTeams():
+    return jsonify(dbManager.getAllTeams()), 200
 
 @app.route('/programs')
 def getPrograms():
     return jsonify(dbManager.getAllPrograms()), 200
+
+@app.route('/contacts')
+def getContacts():
+    return jsonify(dbManager.getAllContacts()), 200
