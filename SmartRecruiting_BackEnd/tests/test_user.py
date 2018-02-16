4cc46195cc0ff378931deffe6fa5743c13fa17a5
ant38 <antoine.gambro@outlook.com>
2018-02-16 14:47:13 +0100

    correction noms attributs (test unitaire)

--- SmartRecruiting_BackEnd/tests/test_user.py
+++ SmartRecruiting_BackEnd/tests/test_user.py
@@ -7,19 +7,19 @@ dbManager = DatabaseManager()
 class Test_user(unittest.TestCase):
     def test_get_all_users(self):
         dbManager.delete_users()
-        user = User('test_last_name', 'test_first_name', 'test_job', 'test_email', 'test_password', 1)
+        user = User('test_name', 'test_surname', 'test_role', 'test_email', 'test_password', 1)
         user.id = 1
         dB.add(user)
         dB.commit()
-        self.assertEqual(dbManager.get_all_users(), [{'id':1,'lastName':'test_last_name','firstName':'test_first_name','job':'test_job','email':'test_email','password':'test_password','admin':True}])
+        self.assertEqual(dbManager.get_all_users(), [{'id':1,'name':'test_name','surname':'test_surname','role':'test_role','email':'test_email','password':'test_password','is_admin':True}])
         
     def test_get_user_by_id(self):
         dbManager.delete_users()
-        user = User('test_last_name', 'test_first_name', 'test_job', 'test_email', 'test_password', 1)
+        user = User('test_name', 'test_surname', 'test_role', 'test_email', 'test_password', 1)
         user.id = 1
         dB.add(user)
         dB.commit()
-        self.assertEqual(dbManager.get_user_by_id(1), {'id':1,'lastName':'test_last_name','firstName':'test_first_name','job':'test_job','email':'test_email','password':'test_password','admin':True})
+        self.assertEqual(dbManager.get_user_by_id(1), {'id':1,'name':'test_name','surname':'test_surname','role':'test_role','email':'test_email','password':'test_password','is_admin':True})
 
 if __name__ == '__main__':
     unittest.main()
