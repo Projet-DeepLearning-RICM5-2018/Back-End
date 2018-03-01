64fa0e6d527388772b75ef758afad83434ec54c9
Cathy <540596775@qq.com>
2018-03-01 11:04:35 +0100

    Train with TensorFlow DONE
    il faut evaluer et comprendre SUMMARIES...

---
+++ SmartRecruiting_DeepLearning/trainW2V.py
@@ -0,0 +1,173 @@
+#! /usr/bin/env python
+
+import tensorflow as tf
+import numpy as np
+import os
+import pretraitement
+import time
+import datetime
+
+from TextCNN import TextCNN 
+
+embedding_dim=128
+filter_sizes="3,4,5"
+num_filters=128
+l2=0.0
+num_checkpoints=5
+dropout_keep_prob=0.5
+checkpoint_every=100
+evaluate_every=100
+batch_size=64
+num_epochs=200
+
+
+base = pretraitement.init()
+#base.shape(59,) array([(,,),(),(),()*59])
+x,y=[],[]
+for i in base:#[(text,preprocess(text),label)]
+    x.append(i[1])
+    y.append(i[2])#base[:,2]
+print("try to find")
+print(base[58][2])
+# Randomly shuffle data
+np.random.seed(10)
+shuffle_indices = np.random.permutation(np.arange(len(y)))
+#print(shuffle_indices)
+x_shuffled = np.array(list(x))[shuffle_indices]
+y_shuffled = np.array(list(y))[shuffle_indices]
+
+#train/test
+dev_sample_index = -1 * int(.1 * float(len(y)))
+x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
+y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
+print("vocabulary size: %s" % len(x_train[0]))
+#cross validation
+k=10
+cut_size = len(x_shuffled) / k
+'''
+for step in range(0,k) :
+    cut = step * cut_size
+    test_x,test_y = x_shuffled[cut:cut+cut_size],y_shuffled[cut:cut+cut_size]
+    learn_x,learn_y = np.vstack((x_shuffled[0:cut], x_shuffled[cut+cut_size:size])),np.vstack((y_shuffled[0:cut], y_shuffled[cut+cut_size:size]))
+'''
+del x, y, x_shuffled, y_shuffled
+print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
+
+# Training
+# ==================================================
+
+with tf.Graph().as_default():
+    with tf.Session() as sess:
+        cnn = TextCNN(
+            sequence_length=pretraitement.max_size,
+            num_classes=3,
+            vocab_size=len(x_train[0]),
+            embedding_size=embedding_dim,
+            filter_sizes=list(map(int, filter_sizes.split(","))),
+            num_filters=num_filters,
+            l2_reg_lambda=l2)
+        # Define Training procedure
+        global_step = tf.Variable(0, name="global_step", trainable=False)
+        optimizer = tf.train.AdamOptimizer(1e-3)
+        grads_and_vars = optimizer.compute_gradients(cnn.loss)
+        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+        
+        # Output directory for models and summaries
+        timestamp = str(int(time.time()))
+        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+        print("Writing to {}\n".format(out_dir))
+
+        # Summaries for loss and accuracy
+        loss_summary = tf.summary.scalar("loss", cnn.loss)
+        acc_summary = tf.summary.scalar("accuracy", cnn.accuracy)
+
+        # Train Summaries
+        train_summary_op = tf.summary.merge([loss_summary, acc_summary])
+        train_summary_dir = os.path.join(out_dir, "summaries", "train")
+        train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+
+        # Dev summaries
+        dev_summary_op = tf.summary.merge([loss_summary, acc_summary])
+        dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
+        dev_summary_writer = tf.summary.FileWriter(dev_summary_dir, sess.graph)
+        
+        
+        # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
+        checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+        checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=num_checkpoints)
+
+
+
+        # Initialize all variables
+        sess.run(tf.global_variables_initializer())
+
+        def train_step(x_batch, y_batch):
+            """
+            A single training step
+            """
+            feed_dict = {
+              cnn.input_x: x_batch,
+              cnn.input_y: y_batch,
+              cnn.dropout_keep_prob: dropout_keep_prob
+            }
+            _, step, summaries, loss, accuracy = sess.run(
+                [train_op, global_step, train_summary_op, cnn.loss, cnn.accuracy],
+                feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            train_summary_writer.add_summary(summaries, step)
+
+        def dev_step(x_batch, y_batch, writer=None):
+            """
+            Evaluates model on a dev set
+            """
+            feed_dict = {
+              cnn.input_x: x_batch,
+              cnn.input_y: y_batch,
+              cnn.dropout_keep_prob: 1.0
+            }
+            step, summaries, loss, accuracy = sess.run(
+                [global_step, dev_summary_op, cnn.loss, cnn.accuracy],
+                feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            if writer:
+                writer.add_summary(summaries, step)
+
+    def batch_iter(data, batch_size, num_epochs, shuffle=True):
+        """
+        Generates a batch iterator for a dataset.
+        """
+        data = np.array(data)
+        data_size = len(data)
+        num_batches_per_epoch = int((len(data)-1)/batch_size) + 1
+        for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+            if shuffle:
+                shuffle_indices = np.random.permutation(np.arange(data_size))
+                shuffled_data = data[shuffle_indices]
+            else:
+                shuffled_data = data
+            for batch_num in range(num_batches_per_epoch):
+                start_index = batch_num * batch_size
+                end_index = min((batch_num + 1) * batch_size, data_size)
+                yield shuffled_data[start_index:end_index]
+
+        # Generate batches
+        batches = batch_iter(
+            list(zip(x_train, y_train)), batch_size, num_epochs)
+        # Training loop. For each batch...
+        for batch in batches:
+            x_batch, y_batch = zip(*batch)
+            train_step(x_batch, y_batch)
+            current_step = tf.train.global_step(sess, global_step)
+            if current_step % evaluate_every == 0:
+                print("\nEvaluation:")
+                dev_step(x_dev, y_dev, writer=dev_summary_writer)
+                print("")
+            if current_step % checkpoint_every == 0:
+                path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+                print("Saved model checkpoint to {}\n".format(path))
